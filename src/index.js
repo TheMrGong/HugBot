cf1d63ce9d27f3bf0522f55600a17047b5de1778
TheMrGong <github-2LbyLBmzs4seOXOY@gon.gg>
2019-02-27 16:09:20 +0000

    Use a config for the bot token

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const config = require("./config.json");
 const client = new Discord.Client();
 
 client.on("ready", () => {
@@ -11,5 +12,5 @@ client.on("message", msg => {
   }
 });
 
-client.login("NDU4MDM1NjQ1Mzk1MjM4OTM2.D1hA0Q.HciJegmyEf_CFBouVN-yuKxoXyg");
+client.login(config.token);
 console.log("Did things.");
