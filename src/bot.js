5fbb738ad4ac5b62381f0e6ad22e4fa2ba030d75
TheMrGong <gongora656@gmail.com>
2019-03-02 14:53:27 -0500

    Make the bot actually use MySQL

--- src/bot.js
+++ src/bot.js
@@ -14,16 +14,31 @@ client.on("ready", () => {
 });
 
 client.on("message", event => {
-  if (event.author.bot || !event.content.toLowerCase().startsWith("?hug"))
+  if (event.author.bot && event.embeds.length > 0 && event.embeds[0].description.toLowerCase().includes("?hug")) {
+    event.delete()
+    return
+  }
+  if (event.author.bot || !event.content.toLowerCase().startsWith("?"))
     return;
+
+
   const spaces = event.content.split(" ");
-  if (spaces.length === 1) {
+  const cmd = spaces[0].toLowerCase().substring(1, spaces[0].length)
+  const args = [];
+  for (let k in spaces) {
+    if (k > 0) args.push(spaces[k]);
+  }
+  if (cmd == "hug")
+    hugCommand(event, args)
+  else if (cmd == "hugs")
+    hugStatsCommand(event, args)
+});
+
+function hugCommand(event, args) {
+  if (args.length == 0) {
     hugTheBot(event);
   } else {
-    const args = [];
-    for (let k in spaces) {
-      if (k > 0) args.push(spaces[k]);
-    }
+
     const targetting = args.join(" ");
     const user = event.mentions.users.first();
 
@@ -46,12 +61,59 @@ client.on("message", event => {
         if (member) hugUser(event, member.user);
         else
           event.channel.send(
-            event.author.toString() + ", your hug failed! Who are you hugging?"
+            getMessageFor("hug-fail", "user", event.author.toString(), "hugging", targetting)
           );
       });
     }
   }
-});
+}
+
+function hugStatsCommand(event, args) {
+  event.delete()
+  if (args.length == 0) {
+    showHugStatsFor(event, event.member);
+  } else {
+
+    const targetting = args.join(" ");
+    const member = event.mentions.members.first();
+
+    if (member) {
+      showHugStatsFor(event, member);
+    } else {
+      event.guild.fetchMembers(targetting, 1).then(() => {
+        let member = findUserBy(
+          event,
+          targetting,
+          member => member.displayName
+        );
+        if (!member)
+          member = findUserBy(
+            event,
+            targetting,
+            member => member.user.username
+          );
+
+        if (member) showHugStatsFor(event, member);
+        else
+          event.channel.send(
+            getMessageFor("hug-stats-nofind", "user", event.author.toString(), "finding", targetting)
+          );
+      });
+    }
+  }
+}
+
+async function showHugStatsFor(event, member) {
+  const stats = await storage.getUserInfo(event.guild.id, member.id)
+  const self = event.author.id === member.id
+  if (!stats) {
+    return event.channel.send(getMessageFor(self ? "hug-stats-self-never" : "hug-stats-never-hugged", "user", event.author.toString(), "found", member.displayName))
+  }
+  const hugs = stats.hugsReceived + " hug" + (stats.hugsReceived == 1 ? "" : "s")
+  if (self) {
+    event.channel.send(getMessageFor("hug-stats-self-received", "user", event.author.toString(), "hugs", hugs))
+  } else event.channel.send(getMessageFor("hug-stats-other-received", "user", event.author.toString(), "hugs", hugs, "found", member.displayName))
+}
 
 function getMessageFor(key, params) {
   const availableMessages = messages[key];
@@ -63,11 +125,9 @@ function getMessageFor(key, params) {
     if (k === 0) continue;
     if ((k - 1) % 2 === 0) {
       const key = arguments[k];
-      console.log("Got key " + key);
-      const value = arguments[k + 1];
-      console.log("got value " + value);
+      const value = arguments[parseInt(k) + 1];
       if (!value) continue; // no value for the key
-      replace = replace.replace(key, value);
+      replace = replace.replace(`{${key}}`, value);
     }
   }
   return replace;
@@ -87,41 +147,45 @@ function findUserBy(event, finding, transform) {
 function hugTheBot(event) {
   const replyingWith = getMessageFor(
     "bot-hugs",
-    "{user}",
+    "user",
     event.author.toString()
   );
   console.log(
     event.member.displayName + " hugged me! Replying with " + replyingWith
   );
-  event.channel.send(replyingWith);
+  event.channel.send(replyingWith)
 
   storage.logHugEvent(event.guild.id, event.author.id, client.user.id);
 }
 
 function hugUser(event, user) {
   if (event.author.id === user.id) {
+    event.delete()
     event.channel.send(
-      "You strongly hug yourself, increasing your self-happiness"
+      getMessageFor("hug-self", "user", event.author.toString())
     );
     return;
   } else if (user.id === client.user.id) return hugTheBot(event);
-  storage.logHugEvent(event.guild.id, event.author.id, user.id);
+  event.delete()
+  storage.logHugEvent(event.guild.id, event.author.id, user.id)
 
-  event.channel.send(
-    event.author.toString() + " has hugged " + user.toString() + "!!"
-  );
+  if (event.mentions.users.array().length > 0) {
+    event.channel.send(
+      getMessageFor("hug-other", "hugger", event.author.toString(), "hugged", user.toString())
+    );
+  } else {
+    event.guild.fetchMember(user).then(member => {
+      event.channel.send(
+        getMessageFor("hug-other", "hugger", event.author.toString(), "hugged", `\`\`${member.displayName}\`\``)
+      );
+    })
+  }
+
   console.log(event.member.displayName + " has hugged " + user.username);
 
-  storage.getTotalHugsSent(event.guild.id, event.author.id).then(hugs => {
-    console.log(event.author.username + " has sent " + hugs + " hug(s)");
-  });
-
-  storage.getTotalHugsReceived(event.guild.id, user.id).then(hugs => {
-    console.log(user.username + " has received " + hugs + " hug(s)");
-  });
 }
 
-module.exports = function(incomingStorage) {
+module.exports = function (incomingStorage) {
   storage = incomingStorage;
   client.login(config.token);
   console.log("Logging in...");
