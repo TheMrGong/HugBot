2212ad8871cdcb6577df043c493c85885fac5e17
DANIEL GONGORA <gongora656@gmail.com>
2019-03-06 13:07:23 -0500

    Relocate config files

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
+const config = require("./config/config.json");
 const lang = require("./lang.js")
 const tacklehugApi = require("./tacklehug.js")
 
