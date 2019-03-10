6a4652627730a4ca4d9091ded950019f5ef97ac1
TheMrGong <gongora656@gmail.com>
2019-03-10 10:40:26 -0400

    Expand util versatility, decouple functions

--- src/command/util.js
+++ src/command/util.js
@@ -4,47 +4,68 @@ const Discord = require("discord.js")
 /**
  * @param {Discord.Message} event 
  * @param {Array<string>} args 
+ * @returns {Promise<Array<Discord.GuildMember>>}
  */
-async function findMemberInEvent(event, args) {
+async function findAllMembersInEvent(event, args) {
     const mentioned = event.mentions.members.first();
 
-    if (mentioned) return mentioned
+    if (mentioned) return [mentioned]
     const targetting = args.join(" ");
     await event.guild.fetchMembers(targetting, 1)
 
+    return findAllMembersInGuildMatching(event.guild, targetting)
+}
+
+/**
+ * @param {Discord.Message} event 
+ * @param {Array<string>} args 
+ * @returns {Promise<Discord.GuildMember|undefined>}
+ */
+async function findMemberInEvent(event, args) {
+    const members = await findAllMembersInEvent(event, args)
+    if (members.length > 0) return members[0]
+}
+
+/**
+ * 
+ * @param {Discord.Guild} guild 
+ * @param {string} targetting 
+ * @returns {Array<Discord.GuildMember|undefined>}
+ */
+function findAllMembersInGuildMatching(guild, targetting) {
     // find direct match
-    let member = findUserBy(
-        event,
+    let membersFound = findMembersBy(
+        guild,
         targetting,
         member => member.displayName,
         true
     );
     // find includes match
-    if (!member) {
-        member = findUserBy(
-            event,
+    if (membersFound.length == 0) {
+        membersFound = findMembersBy(
+            guild,
             targetting,
             member => member.displayName
         );
     }
     // find direct username
-    if (!member)
-        member = findUserBy(
-            event,
+    if (membersFound.length == 0)
+        membersFound = findMembersBy(
+            guild,
             targetting,
             member => member.user.username,
             true
         );
 
     // find includes username
-    if (!member)
-        member = findUserBy(
-            event,
+    if (membersFound.length == 0)
+        membersFound = findMembersBy(
+            guild,
             targetting,
             member => member.user.username
         );
 
-    if (member) return member
+    if (membersFound.length > 0) return membersFound
 }
 
 /**
@@ -53,23 +74,33 @@ async function findMemberInEvent(event, args) {
  * @param {Discord.GuildMember} transforming
  * @returns {string}
  */
+
 /**
  * 
- * @param {Discord.Message} event 
+ * @param {Discord.Guild} guild 
  * @param {string} finding 
  * @param {transform} transform - How to search for the user
  * @param {boolean} direct Checks the names directly
- * @returns {Discord.GuildMember|undefined}
+ * @returns {Array<Discord.GuildMember>}
  */
-function findUserBy(event, finding, transform, direct = false) {
-    const entries = event.guild.members.entries();
+function findMembersBy(guild, finding, transform, direct = false) {
+    const entries = guild.members.entries();
     let entry = entries.next();
+
+    /**@type {Array<Discord.GuildMember>} */
+    let usersFound = []
     while (entry.value) {
         const name = transform(entry.value[1]);
         if (direct ? name.toLowerCase() == finding.toLowerCase() : name.toLowerCase().includes(finding.toLowerCase()))
-            return entry.value[1];
+            usersFound.push(entry.value[1]);
         entry = entries.next();
     }
+    return usersFound
 }
 
-module.exports = findMemberInEvent
+module.exports = {
+    findAllMembersInEvent,
+    findMemberInEvent,
+    findMembersBy,
+    findAllMembersInGuildMatching
+}
