e5a1850c73e207279f7de42071863cd1c079ca25
TheMrGong <github-2LbyLBmzs4seOXOY@gon.gg>
2019-07-28 14:37:16 -0500

    Remove test code

--- src/bot.js
+++ src/bot.js
@@ -22,23 +22,13 @@ client.on("error", err => {
   console.error(err)
 })
 
-async function begin () {
+async function begin() {
   console.log("Logging in...")
   await client.login(config.token)
 
   client.guilds.forEach(async g => {
     console.log(g.name + " - " + g.id)
   })
-
-  const stewie = client.guilds.get("481910869282783235")
-  const mod = stewie.roles.get("481910962291736576")
-  const flags = Discord.Permissions.FLAGS
-
-  for (let k in flags) {
-    if (mod.hasPermission(flags[k])) {
-      console.log("Mod has: " + k)
-    }
-  }
 
   hal.begin(client)
   commandhandler.setup(client)
