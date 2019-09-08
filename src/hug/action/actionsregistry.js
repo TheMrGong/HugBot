7c3c8b2fe807336adec749333fcc36dbaa23d785
TheMrGong <github-2LbyLBmzs4seOXOY@gon.gg>
2019-09-08 13:16:26 -0500

    Update the hug command to use the new system

--- src/hug/action/actionsregistry.js
+++ src/hug/action/actionsregistry.js
@@ -6,8 +6,11 @@ const { HugActionBuilder, HugActions } = require("./hugaction")
 
 const actions = [
     new HugActionBuilder(HugActions.GLOMP).detect("single").emoji("562080200633090052").build(),
-    new HugActionBuilder(HugActions.POKE).detect("single").emoji("562451954001772552").build()
+    new HugActionBuilder(HugActions.POKE).detect("single").emoji("562451954001772552").build(),
+    new HugActionBuilder(HugActions.HUG).detect("single").emoji("537999431266467870").build()
 ]
+
+const preference = require("../../preference/prefenceapi")
 
 module.exports = {
     /**
@@ -15,6 +18,7 @@ module.exports = {
      * @param {Discord.Client} client 
      */
     register(client) {
+        preference.registerPreference("accept-hugs", "true")
         actions.forEach(action => {
             action.register(client)
         })
